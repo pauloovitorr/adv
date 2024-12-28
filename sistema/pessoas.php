9061ef3ca5c5944c856af1f8c5359fe65f7454ec
pauloovitorr <paulov.pv25@gmail.com>
2024-12-28 11:25:19 -0300

    Criando topo tela pessoas

--- sistema/pessoas.php
+++ sistema/pessoas.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 include_once('./menu_lat.php');
 include_once('./topo.php');
@@ -9,14 +9,16 @@ include_once('./topo.php');
 
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
+
     <link rel="stylesheet" href="./css/geral.css">
 
     <style>
-        .infos_pagina{
+        .infos_pagina {
             /* border: 1px solid red; */
             width: 100%;
             /* height: 50px; */
@@ -27,8 +29,8 @@ include_once('./topo.php');
             gap: 16px;
         }
 
-        
-        .opcoes_funcoes{
+
+        .opcoes_funcoes {
             /* border: 1px solid red; */
             width: 100%;
             /* height: 50px; */
@@ -55,7 +57,7 @@ include_once('./topo.php');
             line-height: 14px;
         }
 
-        .btn_adicionar{
+        .btn_adicionar {
             width: auto;
             height: 40px;
             background-color: #2F81F7;
@@ -72,35 +74,124 @@ include_once('./topo.php');
             cursor: pointer;
         }
 
-        .infos_pagina button img{
-            width: 14px;
+        .div_buscar {
+            height: 40px;
+            display: flex;
+            align-items: center;
+            color: #7D7D7D;
+            padding: 0px 8px;
+            border-radius: 8px;
+            transition: .3s;
+            background-color: #FCFCFC;
+            border: 1px solid rgb(232, 232, 232);
         }
 
-        
-        .btn_adicionar img{
+
+
+        .div_buscar input {
+            height: 100%;
+            width: 100px;
+            padding: 10px 8px;
+            border-radius: 8px;
+            border: none;
+            background-color: transparent;
+            color: #7D7D7D;
+            outline: none;
+            transition: .3s;
+            font-size: 14px;
+        }
+
+        .div_buscar input:focus,
+        .div_buscar input:not(:placeholder-shown) {
+            width: 200px;
+        }
+
+
+        .div_buscar img {
             width: 12px;
         }
 
+
+        .div_buscar select {
+            height: 100%;
+            width: 150px;
+            border-radius: 8px;
+            border: none;
+            background-color: transparent;
+            color: #7D7D7D;
+            outline: none;
+            transition: .3s;
+        }
+
+        .div_buscar i {
+            color: #7D7D7D;
+            font-size: 14px;
+        }
     </style>
 
+
+
 </head>
+
 <body>
     <main class="container_principal">
         <div class="pai_conteudo">
 
             <div class="infos_pagina">
-                <button> <img src="../img/icone_pessoa.png" alt="ícone de pessoa"> 3 Pessoas Cadastradas </button>
-                <button> <img src="../img/icone_pessoa.png" alt="ícone de pessoa"> 3 Pessoas Cadastradas </button>
-                <button> <img src="../img/icone_pessoa.png" alt="ícone de pessoa"> 3 Pessoas Cadastradas </button>
+                <button> <i class="fa-regular fa-user"></i> 3 Pessoas Cadastradas </button>
+                <button> <i class="fa-regular fa-user"></i> 3 Pessoas Cadastradas </button>
+                <button> <i class="fa-regular fa-user"></i> 3 Pessoas Cadastradas </button>
             </div>
 
             <div class="opcoes_funcoes">
-                <button class="btn_adicionar"> <img src="../img/icone_add.png" alt="ícone de pessoa"> Novo Contato </button>
-                <!-- <button> <img src="../img/icone_pessoa.png" alt="ícone de pessoa"> 3 Pessoas Cadastradas </button>
-                <button> <img src="../img/icone_pessoa.png" alt="ícone de pessoa"> 3 Pessoas Cadastradas </button> -->
+                <button class="btn_adicionar"> <i class="fa-solid fa-plus"></i> Novo Contato </button>
+
+                <div class="div_buscar">
+                    <label for="buscar_pessoas"><i class="fa-solid fa-magnifying-glass"></i></label>
+                    <input type="text" id="buscar_pessoas" name="buscar_pessoas" placeholder="Buscar">
+                </div>
+
+                <div class="div_buscar">
+                    <img src="../img/funil.png" alt="">
+                    <select name="" id="a">
+                        <option value="">Filtrar</option>
+                        <option value="aaa">Clientes</option>
+                        <option value="aaa">Partes Contrárias</option>
+                        <option value="aa">Com Processso em Andamento</option>
+                        <option value="aaaa"> Sem Processso em Andamento </option>
+                    </select>
+                </div>
+
+
+                <div class="div_buscar">
+                    <img src="../img/funil.png" alt="">
+                    <select name="" id="a">
+                        <option value="">ordenar</option>
+                        <option value="aaa">Nome da Pessoa</option>
+                        <option value="aa">Mais Recentes</option>
+                        <option value="aaaa">Mais Antigos</option>
+                    </select>
+                </div>
+
+                <div class="div_buscar">
+                    <img src="../img/funil.png" alt="">
+                    <select name="" id="a">
+                        <option value="">Exportar</option>
+                        <option value="aaa">Excel (.xlsx)</option>
+                        <option value="aa">Imprimir (.pdf)</option>
+                    </select>
+                </div>
+
+              
             </div>
 
         </div>
     </main>
+
+
+
+
+
 </body>
+
 </html>
--- sistema/topo.php
+++ sistema/topo.php
@@ -5,6 +5,8 @@ include_once('../scripts.php');
 
 ?>
 
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 
 <style>
     .container_topo {
