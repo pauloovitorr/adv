59b6b6b38b56dab7b5835504dc7a96e841e08f77
pauloovitorr <paulov.pv25@gmail.com>
2025-01-03 23:16:25 -0300

    inicio do form

--- sistema/cadastro_pessoa.php
+++ sistema/cadastro_pessoa.php
@@ -27,7 +27,7 @@ include_once('./topo.php');
             /* border: 1px solid red; */
         }
 
-        .etapa{
+        .etapa {
             /* border: 1px solid red; */
             width: auto;
             display: flex;
@@ -41,7 +41,7 @@ include_once('./topo.php');
             width: 50px;
             height: 50px;
             border-radius: 50%;
-            background-color:#D9D9D9;
+            background-color: #D9D9D9;
             display: flex;
             align-items: center;
             justify-content: center;
@@ -51,26 +51,122 @@ include_once('./topo.php');
             font-weight: 500;
         }
 
-       
+
         .descricao {
             color: #D9D9D9;
             font-size: 18px;
         }
 
-        .separador{
+        .separador {
             width: 20%;
             height: 1px;
-            background-color:#D9D9D9;
-            
+            background-color: #D9D9D9;
+
         }
 
 
-        .bg_selecionado{
+        .bg_selecionado {
             background-color: var(--azul-fundo);
         }
 
-        .color_selecionado{
+        .color_selecionado {
             color: var(--preto-primario);
+        }
+
+        .container_cadastro {
+            width: 100%;
+            height: auto;
+            /* padding: 16px; */
+            /* border: 1px solid red; */
+            margin-top: 24px;
+            border-radius: 8px;
+            background-color: white;
+        }
+
+        .topo_sessao {
+            width: 100%;
+            height: 80px;
+            padding: 16px;
+            display: flex;
+            align-items: center;
+            gap: 18px;
+        }
+
+        .topo_sessao i {
+            color: var(--azul-fundo);
+            font-size: 32px;
+        }
+
+        .topo_sessao p {
+            color: var(--azul-fundo);
+            font-size: 24px;
+        }
+
+        hr {
+            height: 1px;
+            background-color: #F1F1F1;
+            border: none;
+        }
+
+        .container_field_form {
+            height: auto;
+            width: 100%;
+            padding: 16px;
+        }
+
+        fieldset {
+            padding: 16px 50px;
+            border-radius: 8px;
+           border: 1px solid  #F1F1F1;;
+        }
+
+        .bloco-formulario{
+            padding: 0px 8px;
+          
+        }
+
+        fieldset legend {
+            width: auto;
+            padding: 0px 8px;
+            color: var(--azul-fundo);
+            font-size: 20px;
+        }
+
+
+        .container_inputs{
+            width: 100%;
+            height: 80px;
+            /* border: 1px solid red; */
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+        }
+
+        .container_input{
+            min-width: 20%;
+            height: auto;
+            /* border: 1px solid red; */
+            display: flex;
+            flex-direction: column;
+            gap: 4px;
+        }
+
+        .container_input select{
+            height: 35px;
+            border-radius: 5px;
+            border: 1px solid var(--branco-secundario)
+        }
+
+        .container_input input{
+            height: 35px;
+            border-radius: 5px;
+            border: 1px solid var(--branco-secundario);
+            padding-left: 4px;
+        }
+
+        #nome{
+            width: 300px;
+           
         }
 
     </style>
@@ -100,6 +196,108 @@ include_once('./topo.php');
                     <div class="num">3º</div>
                     <div class="descricao">Finalização</div>
                 </div>
+
+            </section>
+
+            <section class="container_cadastro">
+                <div class="topo_sessao">
+                    <i class="fa-solid fa-user-plus"></i>
+                    <p>Nova Pessoa</p>
+                </div>
+
+                <hr>
+
+                <div class="container_field_form">
+                    <fieldset>
+                        <legend>Dados Pessoais</legend>
+
+                        <div class="bloco-formulario">
+                        
+                            <form action="" method="POST">
+
+                                <div class="container_inputs">
+                                        <div class="container_input">
+                                            <label for="pessoa">Pessoa</label>
+                                            <select name="pessoa" id="pessoa">
+                                                <option value="pf">Pessoa Física</option>
+                                                <option value="pf">Pessoa Jurídica</option>
+                                            </select>
+                                        </div>
+
+                                        <div class="container_input">
+                                            <label for="pessoa">Nome <span style="color: red;">*</span></label>
+                                            <input type="text" name="nome" id="nome" placeholder="EX: Paulo Vitor">
+                                        </div>
+
+                                        <div class="container_input">
+                                            <label for="pessoa">CPF/CNPJ</label>
+                                            <input type="text" name="num_doc"  placeholder="EX: Paulo Vitor">
+                                        </div>
+
+                                        <div class="container_input">
+                                            <label for="pessoa">RG</label>
+                                            <input type="text" name="rg"  placeholder="EX: Paulo Vitor">
+                                        </div>
+                                </div>
+
+                                <div class="container_inputs">
+                                        <div class="container_input">
+                                            <label for="pessoa">Pessoa</label>
+                                            <select name="pessoa" id="pessoa">
+                                                <option value="pf">Pessoa Física</option>
+                                                <option value="pf">Pessoa Jurídica</option>
+                                            </select>
+                                        </div>
+
+                                        <div class="container_input">
+                                            <label for="pessoa">Nome <span style="color: red;">*</span></label>
+                                            <input type="text" name="nome" id="nome" placeholder="EX: Paulo Vitor">
+                                        </div>
+
+                                        <div class="container_input">
+                                            <label for="pessoa">CPF/CNPJ</label>
+                                            <input type="text" name="num_doc"  placeholder="EX: Paulo Vitor">
+                                        </div>
+
+                                        <div class="container_input">
+                                            <label for="pessoa">RG</label>
+                                            <input type="text" name="rg"  placeholder="EX: Paulo Vitor">
+                                        </div>
+                                </div>
+
+                                <div class="container_inputs">
+                                        <div class="container_input">
+                                            <label for="pessoa">Pessoa</label>
+                                            <select name="pessoa" id="pessoa">
+                                                <option value="pf">Pessoa Física</option>
+                                                <option value="pf">Pessoa Jurídica</option>
+                                            </select>
+                                        </div>
+
+                                        <div class="container_input">
+                                            <label for="pessoa">Nome <span style="color: red;">*</span></label>
+                                            <input type="text" name="nome" id="nome" placeholder="EX: Paulo Vitor">
+                                        </div>
+
+                                        <div class="container_input">
+                                            <label for="pessoa">CPF/CNPJ</label>
+                                            <input type="text" name="num_doc"  placeholder="EX: Paulo Vitor">
+                                        </div>
+
+                                        <div class="container_input">
+                                            <label for="pessoa">RG</label>
+                                            <input type="text" name="rg"  placeholder="EX: Paulo Vitor">
+                                        </div>
+                                </div>
+
+                            </form>
+
+                        </div>
+
+                    </fieldset>
+                </div>
+
+              
 
             </section>
 
